5d20603e592f58bd73144e92c21ac1e8aa51ee97
Sashko Stubailo <sashko@stubailo.com>
2017-04-15 13:59:15 -0700

    Set up mocked canvas schema

--- server/main.js
+++ server/main.js
@@ -1,22 +1,53 @@
 import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import { graphqlConnect, graphiqlConnect } from 'graphql-server-express';
 import { json } from 'body-parser';
+import { integer } from 'casual';
 
 const typeDefs = `
 type Query {
-  hello: String
+  canvas(id: ID!): Canvas!
+}
+
+type Canvas {
+  id: ID!
+  size: XYPair!
+  cells: [Cell!]!
+}
+
+type XYPair {
+  x: Int!
+  y: Int!
+}
+
+type Cell {
+  id: ID!
+  color: Color!
+  location: XYPair!
+}
+
+# Color value in RGB, values from 0-255
+type Color {
+  r: Int!
+  g: Int!
+  b: Int!
 }
 `;
 
-const resolvers = {
-  Query: {
-    hello: () => 'world'
-  }
-}
+// const resolvers = {
+//   Query: {
+//     hello: () => 'world'
+//   }
+// }
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs });
+
+const mocks = {
+  Int: () => integer(0, 255)
+};
+
+addMockFunctionsToSchema({ schema, mocks });
 
 WebApp.connectHandlers.use(json());
 
